9d372525cecb40097e14dcbac5b09a94f820bf1a
joliejyss <101139208+joliejyss@users.noreply.github.com>
2022-05-03 13:58:12 -0400

    Added kangaroo able to move

---
+++ hungry-animal-joliejyss-main/hungry-animal-joliejyss-main/Kangaroo.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Kangaroo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Kangaroo extends Actor
+{
+    /**
+     * Act - do whatever the Kangaroo wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        if (Greenfoot.isKeyDown("j")){
+            move(-2);
+        }
+        if (Greenfoot.isKeyDown("l")){
+            move(2);
+        }
+    }    
+}
