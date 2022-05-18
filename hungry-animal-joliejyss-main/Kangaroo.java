bc73539abcc17781e3666ece2304abb2644f8a0d
joliejyss <101139208+joliejyss@users.noreply.github.com>
2022-05-18 13:59:36 -0400

    Added bubble pop sound

--- hungry-animal-joliejyss-main/Kangaroo.java
+++ hungry-animal-joliejyss-main/Kangaroo.java
@@ -8,6 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Kangaroo extends Actor
 {
+    GreenfootSound bubblePop = new GreenfootSound("QKTA234-pop.mp3");
     /**
      * Act - do whatever the Kangaroo wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -24,11 +25,13 @@ public class Kangaroo extends Actor
         if (Greenfoot.isKeyDown("space")){
             turn(2);
         }
+        
         if (isTouching(Cherry.class)){
             removeTouching(Cherry.class);
             MyWorld world = (MyWorld) getWorld();
             world.increaseScore();
             world.spawnCherry();
+            bubblePop.play();
         }
         
     }
