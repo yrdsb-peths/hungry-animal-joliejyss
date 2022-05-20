36aaa655d901131141c75edec27edaa1eaa3251f
joliejyss <101139208+joliejyss@users.noreply.github.com>
2022-05-20 13:51:15 -0400

    Added my own kangaroo sprite. It is animated and it moves facing left/right

--- hungry-animal-joliejyss-main/Kangaroo.java
+++ hungry-animal-joliejyss-main/Kangaroo.java
@@ -9,18 +9,56 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Kangaroo extends Actor
 {
     GreenfootSound bubblePop = new GreenfootSound("QKTA234-pop.mp3");
+    GreenfootImage[] idleRight = new GreenfootImage[11];
+    GreenfootImage[] idleLeft = new GreenfootImage[11];
+    
+    String facing = "right";
     /**
      * Act - do whatever the Kangaroo wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    public Kangaroo()
+    {
+        for (int i = 0; i < 11; i++)
+        {
+            idleRight[i] = new GreenfootImage("images/idle" + i + ".png.png");
+            idleRight[i].scale(120,100);
+        }        
+        for (int i = 0; i<idleLeft.length; i++)
+        {
+            idleLeft[i] = new GreenfootImage("images/idle" + i + ".png.png");
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(120,100);
+        }
+    }
+    int imageIndex = 0;
+    public void animateKangaroo()
+    {
+        if(facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        }
+        else 
+        {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
+        
+    }
+    
+    
     public void act() 
     {
         // Add your action code here.
         if (Greenfoot.isKeyDown("a")){
             move(-3);
+            facing = "left";
         }
         if (Greenfoot.isKeyDown("d")){
             move(3);
+            facing = "right";
         }
         if (Greenfoot.isKeyDown("space")){
             turn(2);
@@ -34,5 +72,6 @@ public class Kangaroo extends Actor
             world.spawnCherry();
         }
         
+        animateKangaroo();
     }
 }
