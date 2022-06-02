dbd707f27a5849807eb7329191503f4dc2ff24dd
Stewart Chan <stew.chan@gmail.com>
2022-06-02 13:21:19 -0400

    fixed files

--- MyWorld.java
+++ MyWorld.java
@@ -13,9 +13,49 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    public int score = 0;
+    public Label scoreLabel = new Label(score, 80);
+    
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1, false); 
+
+        Kangaroo hello = new Kangaroo();
+        addObject(hello, 300, 350);
+
+        spawnCherry();
+
+        addObject (scoreLabel, 50, 50);
+        
+        
+        
+    }
+
+    public void spawnCherry()
+    {
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+
+        Cherry yes = new Cherry();
+        addObject(yes, x, y);
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+    }
+    
+    public void increaseScore(){
+        score++;
+        scoreLabel.setValue(score);
+    }
+    
+    public void gameOver(){
+        Label gameOverLabel = new Label ("Game Over!", 100);
+        addObject(gameOverLabel, 300, 200);
     }
 }
